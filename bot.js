d1d815b64db3b927c73f329a50bb8f80e55f0e20
tera haz <craphazzard@gmail.com>
2019-08-05 23:03:48 -0400

    welcome to the world, your name is gil

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+    if (/^!spongecase/i.test(msg.content)) {
+        let tokens = msg.content.split(' ')
+        tokens.shift();
+        let str = tokens.join(' ');
+
+
+         // here's the dang algorithm
+        let counter = 0;
+        let spongeArray = [];
+        str.split('').forEach((chr) => {
+            if (/[A-Za-z]/.test(chr)) {
+                if (counter % 2 == 0) {
+                    spongeArray.push(chr.toUpperCase());
+                } else {
+                    spongeArray.push(chr.toLowerCase());
+                }
+                counter = counter + 1;
+
+            } else {
+                spongeArray.push(chr);
+            }
+        });
+        spongeString = spongeArray.join('');
+        msg.channel.send(spongeString);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
