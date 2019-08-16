5aca28cd4b305be3810304e990303d5fab4b8489
tera haz <craphazzard@gmail.com>
2019-08-16 00:21:55 -0400

    summon command for gil

--- bot.js
+++ bot.js
@@ -40,7 +40,6 @@ client.on('message', msg => {
 });
 
 client.on('message', async (msg) => {
-
   if (/^!compliment/i.test(msg.content)) {
 
     const fetch = require('node-fetch');
@@ -48,6 +47,27 @@ client.on('message', async (msg) => {
     let data = await response.json();
 
     await msg.channel.send(data.compliment);
+  }
+});
+
+client.on('message', async (msg) => {
+  if (/^!summon/i.test(msg.content)) {
+      // removes command
+      let tokens = msg.content.split(' ')
+      tokens.shift();
+      let str = tokens.join(' ');
+
+      const useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+      const fetch = require('node-fetch');
+      const jsdom = require('jsdom')
+      const { JSDOM } = jsdom;
+
+      let response = await fetch (`https://www.google.com/search?q=${encodeURI(str)}&tbm=isch`, { headers: {"User-Agent": useragent }})
+      let html = await response.text()
+      let dom = new JSDOM(html)
+      let imageurl = JSON.parse(dom.window.document.getElementsByClassName('rg_meta').item(0).innerHTML).ou;
+
+      await msg.channel.send(`**SUMMONED ${str.toUpperCase()}!**\n${imageurl}`);
   }
 });
 
